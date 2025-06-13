261d76f968abf4f9c2e442d8b011e730c29e0a1f
Taras Nurko <tarasnurko84@gmail.com>
2025-06-13 18:36:42 +0300

    feat: add deposit/withdraw funcitonality

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,20 +1,112 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Counter is ERC20 {
-    uint256 public number;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-    constructor() ERC20("Counter", "CTR") {
-        _mint(msg.sender, 1000000000000000000000000);
+contract PaperProtocol is ERC721, Ownable {
+    using SafeERC20 for IERC20;
+
+    error TokenAmountCannotBeZero(address token);
+    error EtherAmountMustBeZeroForTokenDeposit(
+        address token,
+        uint256 etherAmount
+    );
+    error CallerIsNotNFTOwner(address caller, address owner);
+    error TokenAlreadyWithdrawn(uint256 tokenId);
+    error EtherTransferFailed(uint256 tokenId);
+    event Deposited(
+        uint256 indexed tokenId,
+        address indexed token,
+        uint256 amount,
+        uint256 priceTarget
+    );
+
+    struct Deposit {
+        address token;
+        uint256 amount;
+        uint256 priceTarget;
+        uint256 timestamp; // deposited at
+        bool isPreset;
     }
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    mapping(address token => mapping(uint256 amount => mapping(uint256 priceTarget => bool)))
+        public isPreset;
+
+    mapping(uint256 tokenId => bool) public isWithdrawn;
+
+    mapping(uint256 => Deposit) public deposits;
+
+    uint256 public nextTokenId = 1;
+
+    constructor() ERC721("Paper Protocol", "PP") Ownable(msg.sender) {}
+
+    function deposit(
+        address token,
+        uint256 amount,
+        uint256 priceTarget
+    ) public payable {
+        bool isPreset_ = isPreset[token][amount][priceTarget];
+
+        if (token == address(0)) {
+            amount = msg.value;
+        }
+
+        if (amount == 0) {
+            revert TokenAmountCannotBeZero(token);
+        }
+
+        if (token != address(0)) {
+            if (msg.value != 0) {
+                revert EtherAmountMustBeZeroForTokenDeposit(token, msg.value);
+            }
+
+            IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        }
+
+        deposits[nextTokenId] = Deposit({
+            token: token,
+            amount: amount,
+            priceTarget: priceTarget,
+            timestamp: block.timestamp,
+            isPreset: isPreset_
+        });
+
+        _mint(msg.sender, nextTokenId);
+
+        nextTokenId++;
     }
 
-    function increment() public {
-        number++;
+    function withdraw(uint256 tokenId) public {
+        if (isWithdrawn[tokenId]) {
+            revert TokenAlreadyWithdrawn(tokenId);
+        }
+
+        address nftOwner = _requireOwned(tokenId);
+
+        if (msg.sender != nftOwner) {
+            revert CallerIsNotNFTOwner(msg.sender, nftOwner);
+        }
+
+        isWithdrawn[tokenId] = true;
+
+        Deposit memory userDeposit = deposits[tokenId];
+
+        if (userDeposit.token == address(0)) {
+            (bool success, ) = payable(msg.sender).call{
+                value: userDeposit.amount
+            }("");
+            if (!success) {
+                revert EtherTransferFailed(tokenId);
+            }
+        } else {
+            IERC20(userDeposit.token).safeTransfer(
+                msg.sender,
+                userDeposit.amount
+            );
+        }
     }
 }
